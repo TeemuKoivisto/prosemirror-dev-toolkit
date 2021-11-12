520882ad18f936b327a0cc564928cae6f199d5d1
Teemu Koivisto <tkoivisto456@gmail.com>
2021-11-12 14:39:43 +0200

    [feat] add UMD bundle to enable use as stand-alone script

--- core/rollup.config.js
+++ core/rollup.config.js
@@ -6,7 +6,7 @@ import svelte from 'rollup-plugin-svelte'
 import autoPreprocess from 'svelte-preprocess'
 import postcss from 'rollup-plugin-postcss'
 import babel from 'rollup-plugin-babel'
-// import { terser } from 'rollup-plugin-terser'
+import { terser } from 'rollup-plugin-terser'
 
 import path from 'path'
 
@@ -14,21 +14,72 @@ import pkg from './package.json'
 import svelteConfig from './svelte.config'
 
 const isProduction = !process.env.ROLLUP_WATCH
+const createBundle = process.env.UMD_BUNDLE
 
-export default {
+const plugins = [
+  alias({
+    entries: [
+      { find: '$components', replacement: path.resolve(__dirname, 'src/components') },
+      { find: '$context', replacement: path.resolve(__dirname, 'src/context') },
+      { find: '$stores', replacement: path.resolve(__dirname, 'src/stores') },
+      { find: '$tabs', replacement: path.resolve(__dirname, 'src/tabs') },
+      { find: '$typings', replacement: path.resolve(__dirname, 'src/typings') }
+    ]
+  }),
+  svelte({
+    compilerOptions: {
+      // enable run-time checks when not in production
+      dev: !isProduction
+    },
+    preprocess: autoPreprocess(svelteConfig.preprocessOptions)
+  }),
+  resolve({
+    browser: true,
+    mainFields: ['svelte', 'module', 'browser', 'main'],
+    dedupe: ['svelte']
+  }),
+  // Fun fact: if commonjs() is after typescript() the build fails when _both_ bundles are used.
+  // When using just the other bundle eg just running `yarn watch` all goes fine. How nice is that
+  commonjs(),
+  typescript(),
+  babel({
+    extensions: ['.js', '.mjs', '.html', '.svelte'],
+    runtimeHelpers: true,
+    exclude: ['node_modules/@babel/**'],
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          targets: '> 0.25%, not dead'
+        }
+      ]
+    ],
+    plugins: [
+      '@babel/plugin-syntax-dynamic-import',
+      [
+        '@babel/plugin-transform-runtime',
+        {
+          useESModules: true
+        }
+      ]
+    ]
+  }),
+  postcss()
+]
+
+const defaultBundles = {
   input: 'src/index.ts',
   output: [
     {
       file: pkg.main,
-      format: 'umd',
-      name: 'prosemirror-dev-toolkit',
+      format: 'cjs',
       sourcemap: isProduction
     },
     {
       file: pkg.module,
       format: 'es',
       sourcemap: isProduction
-    }
+    },
   ],
   external: [
     // TODO: It seems svelte-tree-view must be bundled together with the dev-toolkit otherwise the React app
@@ -39,55 +90,25 @@ export default {
     // Rollup doesn't like it (and because it's an outdated version)
     'chalk'
   ],
-  plugins: [
-    alias({
-      entries: [
-        { find: '$components', replacement: path.resolve(__dirname, 'src/components') },
-        { find: '$context', replacement: path.resolve(__dirname, 'src/context') },
-        { find: '$stores', replacement: path.resolve(__dirname, 'src/stores') },
-        { find: '$tabs', replacement: path.resolve(__dirname, 'src/tabs') },
-        { find: '$typings', replacement: path.resolve(__dirname, 'src/typings') }
-      ]
-    }),
-    svelte({
-      compilerOptions: {
-        // enable run-time checks when not in production
-        dev: !isProduction
-      },
-      preprocess: autoPreprocess(svelteConfig.preprocessOptions)
-    }),
-    typescript(),
-    resolve({
-      browser: true,
-      mainFields: ['svelte', 'module', 'browser', 'main'],
-      dedupe: ['svelte']
-    }),
-    commonjs(),
-    babel({
-      extensions: ['.js', '.mjs', '.html', '.svelte'],
-      runtimeHelpers: true,
-      exclude: ['node_modules/@babel/**'],
-      presets: [
-        [
-          '@babel/preset-env',
-          {
-            targets: '> 0.25%, not dead'
-          }
-        ]
-      ],
-      plugins: [
-        '@babel/plugin-syntax-dynamic-import',
-        [
-          '@babel/plugin-transform-runtime',
-          {
-            useESModules: true
-          }
-        ]
-      ]
-    }),
-    postcss()
-  ],
+  plugins,
   watch: {
     clearScreen: false
   }
 }
+
+const umdBundle = {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: './dist/bundle.umd.min.js',
+      format: 'umd',
+      name: 'prosemirror-dev-toolkit',
+    },
+  ],
+  external: [
+    'chalk'
+  ],
+  plugins: [...plugins, terser()],
+}
+
+export default createBundle ? umdBundle : defaultBundles
