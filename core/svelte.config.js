dd856143bd5361297163e8c3f86ba07ffb96d213
Teemu Koivisto <tkoivisto456@gmail.com>
2021-11-18 10:05:42 +0200

    [refactor] remove unused SvelteKit config in svelte.config

--- core/svelte.config.js
+++ core/svelte.config.js
@@ -1,5 +1,4 @@
 import autoPreprocess from 'svelte-preprocess'
-import { resolve } from 'path'
 
 const preprocessOptions = {
   scss: { prependData: `@import 'src/global.scss';` }
@@ -8,18 +7,4 @@ const preprocessOptions = {
 export default {
   preprocess: autoPreprocess(preprocessOptions),
   preprocessOptions,
-  kit: {
-    target: '#svelte',
-    vite: {
-      resolve: {
-        alias: {
-          $components: resolve('./src/components'),
-          $context: resolve('./src/context'),
-          $stores: resolve('./src/stores'),
-          $tabs: resolve('./src/tabs'),
-          $typings: resolve('./src/typings')
-        }
-      }
-    }
-  }
 }
