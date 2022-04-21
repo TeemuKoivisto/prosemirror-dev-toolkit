7d5e975c2d86ad90b5377f2d2a9bba51a6ea2aff
ocavue <ocavue@gmail.com>
2022-04-21 13:54:16 +0800

    fix: don't call applyTransaction before dispatch

---
+++ core/src/history-and-diff/SubscriptionPlugin.ts
@@ -0,0 +1,66 @@
+import type { EditorState, Transaction } from "prosemirror-state";
+import { Plugin, PluginKey } from "prosemirror-state";
+
+const key = new PluginKey("prosemirror-dev-toolkit-transaction-subscription");
+
+class SubscriptionState {
+  private trs: Transaction[] = [];
+
+  constructor() {
+    this.trs = [];
+  }
+
+  push(tr: Transaction) {
+    this.trs.push(tr);
+  }
+
+  clear() {
+    const trs = this.trs;
+    this.trs = [];
+    return trs;
+  }
+}
+
+type SubscriptionPlugin = Plugin<SubscriptionState>
+
+export function initSubscriptionPlugin(): [SubscriptionPlugin, SubscriptionState] {
+  const pluginState = new SubscriptionState();
+
+  const plugin = new Plugin<SubscriptionState>({
+    key,
+    filterTransaction: tr => {
+      pluginState.push(tr);
+      return true;
+    }
+  });
+
+  return [plugin, pluginState]
+}
+
+export function isSubscriptionPlugin(plugin: Plugin): boolean {
+  return plugin.spec.key === key;
+}
+
+/**
+ * Directly remove the subscription plugin into the editor state.
+ *
+ * This is a hack since EditorState should be treated as immutable normally.
+ * @param state
+ */
+export function removeSubscriptionPlugin(state: EditorState) {
+  for (let i = 0; i < state.plugins.length; i++) {
+    if (isSubscriptionPlugin(state.plugins[i])) {
+      state.plugins.splice(i, 1);
+    }
+  }
+}
+
+/**
+ * Directly insert the subscription plugin into the editor state.
+ *
+ * This is a hack since EditorState should be treated as immutable normally.
+ */
+export function insertSubscriptionPlugin(state: EditorState, plugin: SubscriptionPlugin) {
+  removeSubscriptionPlugin(state);
+  state.plugins.push(plugin);
+}
--- core/src/history-and-diff/subscribeToTransactions.ts
+++ core/src/history-and-diff/subscribeToTransactions.ts
@@ -1,23 +1,36 @@
-import type { EditorView } from 'prosemirror-view'
-import type { Transaction } from 'prosemirror-state'
+import type { Transaction } from "prosemirror-state"
+import type { EditorView } from "prosemirror-view"
+import {
+  initSubscriptionPlugin,
+  insertSubscriptionPlugin,
+  removeSubscriptionPlugin
+} from "./SubscriptionPlugin"
 
-import { appendNewHistoryEntry } from '$stores/stateHistory'
+import { appendNewHistoryEntry } from "$stores/stateHistory"
 
 let active = false,
   resetDispatch: (() => void) | undefined = undefined
 
 export function subscribeToDispatchTransaction(view: EditorView) {
   active = true
-  const oldDispatchFn = view.someProp('dispatchTransaction')?.bind(view)
+  const oldDispatchFn = view.someProp("dispatchTransaction")?.bind(view)
+  const [plugin, pluginState] = initSubscriptionPlugin()
+
   view.setProps({
-    dispatchTransaction(tr: Transaction) {
+    dispatchTransaction: (tr: Transaction) => {
+      insertSubscriptionPlugin(view.state, plugin)
+
       const stateBeforeDispatch = view.state
-      const { state, transactions } = this.state.applyTransaction(tr)
+
       if (oldDispatchFn) {
         oldDispatchFn(tr)
       } else {
-        this.updateState(state)
+        view.updateState(view.state.apply(tr))
       }
+
+      const transactions = pluginState.clear()
+      removeSubscriptionPlugin(view.state)
+
       if (active) {
         appendNewHistoryEntry(transactions, view.state, stateBeforeDispatch)
       }
