aed62a9dfcf0501c2fbbecdda5dddf2b348e02fc
Teemu <tkoivisto456@gmail.com>
2022-04-22 11:25:45 +0300

    fix: monkey patch applyTransaction to prevent multiple apply's

--- core/src/history-and-diff/subscribeToTransactions.ts
+++ core/src/history-and-diff/subscribeToTransactions.ts
@@ -1,27 +1,61 @@
 import type { EditorView } from 'prosemirror-view'
-import type { Transaction } from 'prosemirror-state'
+import type { EditorState, Transaction } from 'prosemirror-state'
 
 import { appendNewHistoryEntry } from '$stores/stateHistory'
 
+type Applied = ReturnType<EditorState['applyTransaction']>
+
 let active = false,
+  applied: Applied | undefined = undefined,
   resetDispatch: (() => void) | undefined = undefined
+
+/**
+ * Handler for dispatchTransaction that pushes trs to history and monkey patches applyTransaction
+ * 
+ * Incase the editor already has a `dispatchTransaction` prop, oldDispatchFn is called but because
+ * that function would also call state.apply we'd run apply multiple times, causing plugins to see
+ * transactions often twice. So we monkey patch the `applyTransaction` method and apply some reflection
+ * magic to maintain this-context as well as remove the patch if the transaction is somehow different
+ * from the original.
+ * 
+ * Why we have to monkey patch the method here and not once globally, is because the method is reset
+ * to the original by ProseMirror at times so the function would not get called. Proxying `view.state.applyTransaction`
+ * didn't also work.
+ * 
+ * The other option is to use Plugins to store the transactions https://github.com/TeemuKoivisto/prosemirror-dev-toolkit/pull/15
+ * which we'll revert to incase this one doesn't cover all the cases.
+ * @param view 
+ * @param oldDispatchFn 
+ * @returns 
+ */
+const handleDispatch =
+  (view: EditorView, oldDispatchFn: (tr: Transaction) => void) =>
+  (tr: Transaction) => {
+    const stateBeforeDispatch = view.state
+    applied = view.state.applyTransaction(tr)
+    if (oldDispatchFn) {
+      const oldFn = view.state.applyTransaction.bind(view.state)
+      view.state.applyTransaction = function (trr) {
+        if (trr !== tr) {
+          view.state.applyTransaction = oldFn
+          return Reflect.apply(oldFn, view.state, arguments)
+        }
+        return applied
+      }
+      oldDispatchFn(tr)
+    } else {
+      view.updateState(applied.state)
+    }
+    if (active && applied) {
+      appendNewHistoryEntry(applied.transactions, view.state, stateBeforeDispatch)
+    }
+  }
 
 export function subscribeToDispatchTransaction(view: EditorView) {
   active = true
   const oldDispatchFn = view.someProp('dispatchTransaction')?.bind(view)
   view.setProps({
-    dispatchTransaction(tr: Transaction) {
-      const stateBeforeDispatch = view.state
-      const { state, transactions } = this.state.applyTransaction(tr)
-      if (oldDispatchFn) {
-        oldDispatchFn(tr)
-      } else {
-        this.updateState(state)
-      }
-      if (active) {
-        appendNewHistoryEntry(transactions, view.state, stateBeforeDispatch)
-      }
-    }
+    dispatchTransaction: handleDispatch(view, oldDispatchFn)
   })
   resetDispatch = () => view.setProps({ dispatchTransaction: oldDispatchFn })
 }
