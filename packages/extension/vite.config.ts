3c950472894be4c9587ff0a254e184df0887c647
Teemu Koivisto <tkoivisto456@gmail.com>
2022-11-09 00:10:50 +0200

    fix: wrap output in iife to not to conflict with other code

--- packages/extension/vite.config.ts
+++ packages/extension/vite.config.ts
@@ -4,6 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 import path from 'path'
 
+/** @type {import('vite').UserConfig} */
 export default defineConfig({
   plugins: [svelte({}), tsconfigPaths()],
   build: {
@@ -17,7 +18,20 @@ export default defineConfig({
         inject: path.resolve('./src/inject.ts'),
         proxy: path.resolve('./src/proxy.ts'),
         'pop-up': path.resolve('./pop-up.html')
-      }
+      },
+      plugins: [
+        {
+          name: 'wrap-in-iife',
+          generateBundle(outputOptions, bundle) {
+            Object.keys(bundle).forEach(fileName => {
+              const file = bundle[fileName]
+              if (fileName.slice(-3) === '.js' && 'code' in file) {
+                file.code = `(() => {\n${file.code}})()`
+              }
+            })
+          }
+        }
+      ]
     }
   }
 })
