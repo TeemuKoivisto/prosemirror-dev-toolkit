fb6d5a96fa6a739e913975a0df3429c494822f4f
Teemu Koivisto <tkoivisto456@gmail.com>
2022-11-28 17:12:29 +0200

    refactor: remove context-menus

--- packages/extension/src/sw/index.ts
+++ packages/extension/src/sw/index.ts
@@ -1,29 +1,4 @@
-import openDevToolsWindow, { DevToolsPosition } from './openWindow'
-import { createMenu, removeMenu } from './contextMenus'
 import { listenToConnections } from './ports'
-
-// Listen for keyboard shortcuts
-chrome.commands.onCommand.addListener(shortcut => {
-  openDevToolsWindow(shortcut as DevToolsPosition)
-})
-
-// Create the context menu when installed
-chrome.runtime.onInstalled.addListener(() => {
-  createMenu()
-  // openDevToolsWindow('devtools-left')
-})
-
-// chrome.management.onEnabled.addListener(
-//   callback: function,
-// )
-
-// Create or Remove context menu when config changed
-chrome.storage.onChanged.addListener(changes => {
-  if (changes.showContextMenus) {
-    if (changes.showContextMenus.newValue) createMenu()
-    else removeMenu()
-  }
-})
 
 chrome.scripting.registerContentScripts([
   {
@@ -34,7 +9,6 @@ chrome.scripting.registerContentScripts([
     world: 'MAIN'
   }
 ])
-
 chrome.runtime.onConnect.addListener(listenToConnections)
 
 export {}
