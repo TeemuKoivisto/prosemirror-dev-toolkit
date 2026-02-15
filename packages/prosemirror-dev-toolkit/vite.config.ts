7ac7f645de640f0e5e3b613c0d62e25d5f376154
Teemu Koivisto <tkoivisto456@gmail.com>
2026-02-15 18:41:17 +0200

    combine dist, umd into one vite.config

--- packages/prosemirror-dev-toolkit/vite.config.ts
+++ packages/prosemirror-dev-toolkit/vite.config.ts
@@ -1,25 +1,40 @@
-/// <reference types="vitest" />
-
 import { defineConfig } from 'vitest/config'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import dts from 'vite-plugin-dts'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 import { resolve } from 'path'
 
-// For tests only
+const UMD_BUNDLE = process.env.UMD_BUNDLE === 'true'
+
 export default defineConfig({
-  plugins: [
-    svelte({
-      dynamicCompileOptions: () => {
-        return {
-          cssHash: ({ hash, css, name, filename }) => {
-            return `s-${name}-${hash(css)}`
-          }
+  build: {
+    outDir: 'dist',
+    ...(UMD_BUNDLE
+      ? {
+          lib: {
+            entry: resolve('src/index.ts'),
+            formats: ['umd'],
+            name: 'prosemirror-dev-toolkit',
+            fileName: () => 'bundle.umd.min.js'
+          },
+          minify: true
         }
-      }
-    }),
-    tsconfigPaths()
-  ],
+      : {
+          lib: {
+            entry: resolve('src/index.ts'),
+            formats: ['es', 'cjs'],
+            fileName: format => {
+              if (format === 'cjs') {
+                return 'index.cjs'
+              } else {
+                return 'index.js'
+              }
+            }
+          },
+          minify: false
+        })
+  },
   resolve: {
     conditions: ['browser'],
     alias: {
@@ -31,6 +46,21 @@ export default defineConfig({
       $typings: resolve('./src/typings')
     }
   },
+  plugins: [
+    dts(),
+    svelte({
+      extensions: ['.svelte'],
+      emitCss: false,
+      dynamicCompileOptions: () => {
+        return {
+          cssHash: ({ hash, css, name, filename }) => {
+            return `s-${name}-${hash(css)}`
+          }
+        }
+      }
+    }),
+    tsconfigPaths()
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
